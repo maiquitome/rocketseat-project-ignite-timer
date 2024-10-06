54e48e3803c0e0fc2f65cc16aaae2afadb7f8369
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-10-06 13:55:15 -0300

    feat: :sparkles: add themes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -5,6 +5,7 @@ export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
+
 const buttonVariants = {
   primary: "purple",
   secondary: "orange",
@@ -15,9 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${(props) => {
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+  /* ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
-  }}
+  }} */
 `;
